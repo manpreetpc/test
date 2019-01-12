436b6de5edb73cb74340417aa57a6a20ed4eac81
Manpreet Kumar <34676908+manpreetpc@users.noreply.github.com>
2019-01-12 17:30:39 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,10 @@
+﻿jQuery(".volunteer a").click(function() {
+	alert("hello");
+    ﻿jQuery('html,body').animate({
+        scrollTop: ﻿jQuery("#volunteer_block").offset().top},
+        'slow');
+});
+
+jQuery(document).load(function(){
+	alert();
+})
